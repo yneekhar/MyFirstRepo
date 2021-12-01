bfb8decd501174c89341b733661183eeaf4aa60f
Simant-Git <58509537+Simant-Git@users.noreply.github.com>
2021-12-01 18:10:56 +0530

    Update Demo.java

--- SampleProject/src/Demo.java
+++ SampleProject/src/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Demo.java cfghfghfhfhfgh");
-
+		System.out.println("made some changes through master branch");
 	}
 
 }
