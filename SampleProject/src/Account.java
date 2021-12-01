ef9ea51563c1175de8932234585b8502b827c88b
yneekhar <58767685+yneekhar@users.noreply.github.com>
2021-12-01 16:26:45 +0530

    Update Account.java

--- SampleProject/src/Account.java
+++ SampleProject/src/Account.java
@@ -3,11 +3,11 @@ public class Account {
 
 	public static void main(String[] args) {
 		System.out.println("Account class working");
-<<<<<<< HEAD
+
 		System.out.println("Code added by Yash ");
-=======
+
 		System.out.println("Added more code in the Account class.");
->>>>>>> branch 'master' of https://github.com/yneekhar/MyFirstRepo.git
+
 	}
 
 }
