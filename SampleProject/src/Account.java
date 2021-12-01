3b0e564c3324cbfcb5a57307b2ff213eb50f5b25
Simant-Git <simant.setu@hotmail.com>
2021-12-01 16:12:14 +0530

    modified Account.java

--- SampleProject/src/Account.java
+++ SampleProject/src/Account.java
@@ -3,7 +3,7 @@ public class Account {
 
 	public static void main(String[] args) {
 		System.out.println("Account class working");
-
+		System.out.println("Added more code in the Account class.");
 	}
 
 }
